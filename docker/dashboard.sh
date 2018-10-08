7a8feff6660e6048b400b0db8f307b34e729c45b
Paul Laffitte <paul.laffitte@epitech.eu>
2018-10-08 23:41:52 +0200

    docker-compose client dev and prod

---
+++ docker/dashboard.sh
@@ -0,0 +1,72 @@
+#! /usr/bin/env bash
+
+function setTarget {
+	export TARGET="$1"
+	COMPOSE_PROJECT_NAME="dashboard${TARGET}"
+
+	if [[ "${TARGET}" != "dev" && "${TARGET}" != "prod" ]]; then
+		>&2 echo "Supported targets are dev and prod"
+	fi
+}
+
+function readTarget {
+	setTarget `cat .dashboard.target 2> /dev/null`
+}
+
+function selectTarget {
+	readTarget
+	while [[ "${TARGET}" != "dev" && "${TARGET}" != "prod" ]]; do
+		read -p "select a target (dev/prod): " target
+		mkdir -p data/
+		echo ${target} > .dashboard.target
+		readTarget
+	done
+}
+
+function check {
+	status=$?
+
+	if [ $status -ne 0 ]
+	then
+		echo "==> Error: command exited with code" $status
+		exit 1
+	fi
+}
+
+function composeFile {
+	if [[ ${TARGET} == "dev" ]]; then
+		echo "./docker/docker-compose.dev.yml"
+	fi
+
+	if [[ ${TARGET} == "prod" ]]; then
+		echo "./docker/docker-compose.yml"
+	fi
+}
+
+function launch {
+	if [[ $1 == "default" ]]; then
+		launch up -d
+		return
+	fi
+
+	docker-compose -p ${COMPOSE_PROJECT_NAME} -f $(composeFile) $@
+}
+
+pushd `pwd` > /dev/null
+cd "`dirname $0`/../"
+
+selectTarget
+
+if [ "$1" == "target" ]; then
+	rm -rf .dashboard.target
+	selectTarget
+elif [ "$1" == "re" ]; then
+	launch build ${@:2}
+	launch default
+elif [ "$1" == "" ]; then
+	launch default
+else
+	launch ${@:1}
+fi
+
+popd > /dev/null
