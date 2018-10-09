753cace1b774a34e10d0281f67fb6bcefdf5a4c1
thmsrbn <thomas.arbona@epitech.eu>
2018-10-09 16:04:12 +0200

    feat(api): store widgets

--- api/src/models/users.model.js
+++ api/src/models/users.model.js
@@ -4,20 +4,23 @@
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
-  const users = new mongooseClient.Schema({
-  
+  const Schema = mongooseClient.Schema;
+  const users = new Schema({
+
     email: {type: String, unique: true},
     password: { type: String },
-  
-  
+
+    widgets: [{
+      id: { type: String, unique: true, required: true },
+      name: { type: String },
+      config: { type: Schema.Types.Mixed },
+    }],
+
     auth0Id: { type: String },
-  
     googleId: { type: String },
-  
     facebookId: { type: String },
-  
     githubId: { type: String },
-  
+
   }, {
     timestamps: true
   });
