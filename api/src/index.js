f4a5c35b186db9d4ede492e88672b5a4de793353
Arthur Chaloin <arthur@achaloin.com>
2018-10-20 15:19:21 +0200

    feat(build): epitech compliant build

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
 const fs = require('fs');
-const https  = require('https');
+const https = require('https');
 const logger = require('winston');
 const app = require('./app');
 
 const server = https.createServer({
-  key: fs.readFileSync('../dashboardHTTPS/server.key'),
-  cert: fs.readFileSync('../dashboardHTTPS/server.csr')
+  key: fs.readFileSync('./tls/server.key'),
+  cert: fs.readFileSync('./tls/server.csr')
 }, app).listen(3030);
 
 // Call app.setup to initialize all services and SocketIO
